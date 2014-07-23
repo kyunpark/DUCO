c660c744b2610d8ec083b29b8edd846133e21b2f
sungkyun <kyun.park88@gmail.com>
2014-07-23 15:19:55 +0100

    tide up
    tide up

--- ColumnGenerator.java
+++
@@ -1,8 +0,0 @@
-package generating;
-
-import java.util.Calendar;
-
-public interface ColumnGenerator {
-	public String generateColumnValue(Calendar[] benchTime, String[] tradeDate,
-			boolean[] tradeDateGenerateflag,boolean[] tradeIdExistence,int rowsNumber);
-}
